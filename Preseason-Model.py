2e80c1cb50d9a4314c705a4f6a1f54cd26f746c2
Chris Hogan <chrishogan99@hotmail.com>
2019-07-20 22:18:32 -0500

    Injury Keys Update

--- Preseason-Model.py
+++ Preseason-Model.py
@@ -143,9 +143,9 @@ for item in all_players:
     #Scans people on Injury Report
     for injury in IR:
         if item[0][0] == injury[0]:
-            if 'IR.' in injury[4] or 'indefinitely' in injury[4]:
+            if 'IR. Injured Reserve' in injury[4] or 'indefinitely' in injury[4] or 'Physically Unable to Perform' in injury[4]:
                 final = 0
-            elif 'Questionable' in injury[4]:
+            elif 'Questionable' in injury[4] or 'Out for the start of training camp' in injury[4]:
                 final = final * 15
                 final_pass = 1
             elif any(char.isdigit() for char in injury[4]):
