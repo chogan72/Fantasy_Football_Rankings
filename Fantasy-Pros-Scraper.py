390591eaff26e2535d7c189caabe72cab9e4d97c
Chris Hogan <chrishogan99@hotmail.com>
2019-07-26 20:54:26 -0500

    Fantasy Pros Rookie Rankings

--- Fantasy-Pros-Scraper.py
+++ Fantasy-Pros-Scraper.py
@@ -23,9 +23,6 @@ change_directory('\\Database\\')
 
 #Fantasy Positions
 positions = ['QB', 'RB', 'WR', 'TE', 'K', 'DST']
-#Player Format
-players = ['Name', 'Position', 'Team', 'Bye']
-database('Fantasy-Pros-Database', players)
 
 #Player Name Fix
 players_skip ={
@@ -40,79 +37,88 @@ players_skip ={
         'Josh Jacobs':'Joshua Jacobs'
         }
 
-#beautifulsoup4 link
-BS_link = 'https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'
-sauce = requests.get(BS_link)
-soup = bs4.BeautifulSoup(sauce.text, 'html.parser')
+files = [['Fantasy-Pros-Database','https://www.fantasypros.com/nfl/rankings/consensus-cheatsheets.php'],
+         ['Fantasy-Pros-Rookies-Database','https://www.fantasypros.com/nfl/rankings/rookies.php']]
 
-#Bye week Variable
-bye_week = 0
+for file in files:
+    #Player Format
+    players = ['Name', 'Position', 'Team', 'Bye']
+    database(file[0], players)
 
-#Pulls Table Data
-for player in soup.find_all('td'):
-    #Splits needed information
-    gdata = (player.text)
-    gdata = re.split('>|<', gdata)
-    gdata = gdata[0]
-    #Bye Week Check
-    if bye_week == 1:
-        players.append(gdata)
-        database('Fantasy-Pros-Database', players)
-        bye_week = 0
-    #Pulls out player data
-    for position in positions:            
-        if str(gdata).startswith(position):
-            
-            #K
-            if position == 'K':
-                #Removes unneeded results
-                kicker = ['K0','K1','K2','K3','K4','K5','K6','K7','K8','K9']
-                for number in kicker:
-                    if str(gdata).startswith(number):
-                        last_line = re.split(' ', last_line)
-                        item = len(last_line) - 2
-                        #Player Name
-                        if '.' in last_line[1]:
-                            current = last_line[0] + ' ' + last_line[1][:-2]
-                        else:
-                            current = last_line[0] + ' ' + last_line[1] + ' ' + last_line[2][:-2]
-                        #Player Information
-                        players = [current, position, last_line[item]]
-                        bye_week = 1
-            
-            #DST
-            elif position == 'DST':
-                last_line = re.split(' ', last_line)
-                #Defense Information
-                if len(last_line) == 4:
-                    players = [last_line[0], position, last_line[1]]
-                else:
-                    players = [last_line[0] + ' ' + last_line[1], position, last_line[2]]
-                #Defense Team
-                if len(players[2]) > 7:
-                    players[2] = players[2][5:]
-                else:
-                    players[2] = players[2][4:]
-                bye_week = 1
-            
-            #QB, RB, WR, TE
-            elif position == 'QB' or position == 'RB' or position == 'WR' or position == 'TE':
-                last_line = re.split(' ', last_line)
-                item = len(last_line) - 2
-                #Player Name
-                if '.' in last_line[1]:
-                    current = last_line[0] + ' ' + last_line[1][:-2]
-                else:
-                    current = last_line[0] + ' ' + last_line[1] + ' ' + last_line[2][:-2]
-                #Player Information
-                players = [current, position, last_line[item]]
-                #Confirms Player
-                if len(players[2]) <= 3 and '.' not in players[2]:
+    #beautifulsoup4 link
+    BS_link = file[1]
+    sauce = requests.get(BS_link)
+    soup = bs4.BeautifulSoup(sauce.text, 'html.parser')
+
+    #Bye week Variable
+    bye_week = 0
+
+    #Pulls Table Data
+    for player in soup.find_all('td'):
+        #Splits needed information
+        gdata = (player.text)
+        gdata = re.split('>|<', gdata)
+        gdata = gdata[0]
+        #Bye Week Check
+        if bye_week == 1:
+            players.append(gdata)
+            database(file[0], players)
+            bye_week = 0
+        #Pulls out player data
+        for position in positions:            
+            if str(gdata).startswith(position):
+                
+                #K
+                if position == 'K':
+                    #Removes unneeded results
+                    kicker = ['K0','K1','K2','K3','K4','K5','K6','K7','K8','K9']
+                    for number in kicker:
+                        if str(gdata).startswith(number):
+                            last_line = re.split(' ', last_line)
+                            item = len(last_line) - 2
+                            #Player Name
+                            if '.' in last_line[1]:
+                                current = last_line[0] + ' ' + last_line[1][:-2]
+                            else:
+                                current = last_line[0] + ' ' + last_line[1] + ' ' + last_line[2][:-2]
+                            #Player Information
+                            players = [current, position, last_line[item]]
+                            bye_week = 1
+                
+                #DST
+                elif position == 'DST':
+                    last_line = re.split(' ', last_line)
+                    #Defense Information
+                    if len(last_line) == 4:
+                        players = [last_line[0], position, last_line[1]]
+                    else:
+                        players = [last_line[0] + ' ' + last_line[1], position, last_line[2]]
+                    #Defense Team
+                    if len(players[2]) > 7:
+                        players[2] = players[2][5:]
+                    else:
+                        players[2] = players[2][4:]
                     bye_week = 1
-                    
-            #Name Fix
-            for skip in players_skip:
-                if players[0] == skip:
-                    players[0] = players_skip[skip]
-    #Saves player info from last line
-    last_line = gdata
+                
+                #QB, RB, WR, TE
+                elif position == 'QB' or position == 'RB' or position == 'WR' or position == 'TE':
+                    last_line = re.split(' ', last_line)
+                    item = len(last_line) - 2
+                    #Player Name
+                    if '.' in last_line[1]:
+                        current = last_line[0] + ' ' + last_line[1][:-2]
+                    else:
+                        current = last_line[0] + ' ' + last_line[1] + ' ' + last_line[2][:-2]
+                    #Player Information
+                    players = [current, position, last_line[item]]
+                    #Confirms Player
+                    if len(players[2]) <= 3 and '.' not in players[2]:
+                        bye_week = 1
+                        
+                #Name Fix
+                for skip in players_skip:
+                    if players[0] == skip:
+                        players[0] = players_skip[skip]
+        #Saves player info from last line
+        last_line = gdata
+
